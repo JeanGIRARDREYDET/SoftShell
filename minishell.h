487fd83c955c2e7806d6f2c8245c36908503ef79
Dollyne Feret <doferet@paul-f5Br5s1.clusters.42paris.fr>
2024-09-06 14:36:36 +0200

    .h

---
+++ minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 12:09:10 by doferet           #+#    #+#             */
+/*   Updated: 2024/09/06 10:18:30 by doferet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#include "/home/doferet/Documents/libft/libft.h"
+
+#endif
