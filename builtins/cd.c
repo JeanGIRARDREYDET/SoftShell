8b3fe1c6416db8b92c2608be5fa60613d8f85cc0
Dollyne Feret <doferet@paul-f5Br5s1.clusters.42paris.fr>
2024-09-10 15:00:18 +0200

    builtins + modif main

---
+++ builtins/cd.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 13:46:02 by doferet           #+#    #+#             */
+/*   Updated: 2024/09/10 14:42:49 by doferet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+//utiliser la fonction getcwd qui copie le chemin d'acces absolu du repertoire 
+//de travail courant dans la chaine pointee par buf qui est de longueur size
+
---
+++ builtins/unset.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 13:48:11 by doferet           #+#    #+#             */
+/*   Updated: 2024/09/10 11:44:01 by doferet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// unset values and attributes of variables and functions
+// unset peut supprimer une variable cree dans env
