4c1a1c314a5cf69b809fdcecf2dc617e14863b55
jeanGIRARDREYDET <cogitafac@gmail.com>
2024-12-06 12:27:34 +0100

    suppressiond de quotes

--- parsing/mi_unbordercote.c
+++ parsing/mi_unbordercote.c
@@ -22,6 +22,9 @@ void   mi_unbordercote(char *args, t_sys *mi_sys)
 	j = 0;
 	if (mi_sys->nb_error > 0 || args== NULL)
 		return ;
+	while (args[i] != '\'' && args[i] != '\"' && args[i] != '\0')
+		i++;
+	j = i;
 	if (args[i] == '\'' || args[i] == '\"')
 		replace = args[i];
 	else
