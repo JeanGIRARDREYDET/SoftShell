059f79bf963b54f82c86705b124e05d48997b34f
jeanGIRARDREYDET <cogitafac@gmail.com>
2024-12-09 14:19:38 +0100

    ok

--- parsing/mi_lexing.c
+++ parsing/mi_lexing.c
@@ -12,49 +12,47 @@
 
 #include "../minishell.h"
 
-void	mi_lexinglineerr(char *ln, t_sys *mi_sys)
-
+bool	mi_lexinglincheckerr(char *ln, t_cmd *mi_cmd, t_sys *mi_sys)
 {
 	char		*msg_err;
 
-	msg_err = join_3("syntax error near ", ln, " unexpected token");
-	mi_sys->nb_pipe = 0;
-	mi_logerror(2, msg_err, mi_sys);
-	free(msg_err);
-	mi_freecmd(mi_sys);
+	if (mi_cmd->full == NULL)
+	{
+		msg_err = join_3("syntax error near ", ln, " unexpected token");
+		mi_sys->nb_pipe = 0;
+		mi_logerror(2, msg_err, mi_sys);
+		free(msg_err);
+		mi_freecmd(mi_sys);
+		return (true);
+	}
+	return (false);
 }
+
 void	mi_lexingline(char *ln, t_sys *mi_sys)
 {
 	t_cmd		*mi_cmd;
-	t_cmd		*new_cmd;
-
-	size_t		start;
-	size_t		i;
+	size_t		i[2];
 
 	mi_sys->nb_pipe = 0;
 	mi_cmd = mi_createcmd(mi_sys);
-	i = 0;
-	start = 0;
+	i[1] = 0;
+	i[0] = 0;
 	mi_sys->cmd = mi_cmd;
-	while (ft_strlen(ln) >= i && ln[i])
+	while (ft_strlen(ln) >= i[1] && ln[i[1]])
 	{
-		if (ft_strin(TECHAP, ln[i]))
-			mi_pospasscote(ln, &i, mi_sys);
-		else if (ln[i] == '|')
+		if (ft_strin(TECHAP, ln[i[1]]))
+			mi_pospasscote(ln, &i[1], mi_sys);
+		else if (ln[i[1]] == '|')
 		{
-			mi_cmd->full = ft_strtrimparam(ln, start, i, WSPACE);
-			if (mi_cmd->full == NULL)
-			{
-				mi_lexinglineerr(ln, mi_sys);
+			mi_cmd->full = ft_strtrimparam(ln, i[0], i[1], WSPACE);
+			if (mi_lexinglincheckerr(ln, mi_cmd, mi_sys))
 				return ;
-			}
-			new_cmd = mi_createcmd(mi_sys);
-			mi_cmd->next = new_cmd;
-			mi_cmd = new_cmd;
-			start = i + 1;
+			mi_cmd->next = mi_createcmd(mi_sys);
+			mi_cmd = mi_cmd->next;
+			i[0] = i[1] + 1;
 		}
-		i++;
+		i[1]++;
 	}
-	mi_cmd->full = ft_strtrimparam(ln, start, i, WSPACE);
+	mi_cmd->full = ft_strtrimparam(ln, i[0], i[1], WSPACE);
 	mi_cmd->next = NULL;
 }
