32e281595389a5ada032a3200afa8744725102c7
jeanGIRARDREYDET <cogitafac@gmail.com>
2024-12-10 18:42:34 +0100

    ok

--- common/mi_sysinitialization.c
+++ common/mi_sysinitialization.c
@@ -74,40 +74,37 @@ void	mi_setdefaul_initialization( t_sys *mi_sys)
 
 void	mi_sysinitializationsave(int i, char ***ienv, t_sys *mi_sys)
 {
-	*ienv = (char **)ft_calloc(i, sizeof(char *));
-	i = -1;
-	while (mi_sys->env[++i])
-		(*ienv)[i] = ft_strdup(mi_sys->env[i]);
-	(*ienv)[i] = NULL;
+	if (mi_sys->senv->pwd == NULL)
+		ft_sys_get_pwd(&mi_sys->senv->pwd);
+	mi_sys->env = *ienv;
+	mi_sys->len_env = i;
+	mi_sys->nb_error = 0;
 }
 
 void	mi_sysinitialization(char **env, t_sys *mi_sys)
 {
 	int		i;
+	char	**ienv;
 	bool	shlvl;
 
 	shlvl = false;
 	mi_setdefaul_initialization(mi_sys);
 	mi_sys->len_env = read_env(env, mi_sys);
-	mi_sys->env = (char **)ft_calloc(mi_sys->len_env, sizeof(char *));
+	ienv = (char **)ft_calloc(mi_sys->len_env, sizeof(char *));
 	i = -1;
-	while (mi_sys->env[++i])
+	while (env[++i])
 	{
 		if (ft_strnstr (env[i], "SHLVL=", 6) != 0 && !shlvl)
 		{
-			mi_sys->env[i] = ft_strjoin("SHLVL=", mi_sys->senv->shlvl);
+			ienv[i] = ft_strjoin("SHLVL=", mi_sys->senv->shlvl);
 			shlvl = true;
 		}
 		if (ft_strnstr (env[i], "SHLVL=", 6) == 0)
-			mi_sys->env[i] = ft_strdup(env[i]);
+			ienv[i] = ft_strdup(env[i]);
 	}
 	if (!shlvl)
-		mi_sys->env[i++] = ft_strjoin("SHLVL=", mi_sys->senv->shlvl);
-//	mi_sysinitializationsave(i, &ienv, mi_sys);
+		ienv[i++] = ft_strjoin("SHLVL=", mi_sys->senv->shlvl);
+	mi_sysinitializationsave(i, &ienv, mi_sys);
 	if (mi_sys->senv->pwd == NULL)
-		ft_sys_get_pwd(&mi_sys->senv->pwd);
-	mi_sys->len_env = i;
-	mi_sys->nb_error = 0;
-	if (mi_sys->senv->pwd == NULL)
-		mi_sys->env[i++] = ft_strjoin("PWD=", mi_sys->senv->pwd);
+		ienv[i++] = ft_strjoin("PWD=", mi_sys->senv->pwd);
 }
