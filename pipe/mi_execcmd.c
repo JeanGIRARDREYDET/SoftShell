ff41d01d8d2af9d520af327604eb967e696f00e9
JeanGIRARDREYDFET <cogitafac@gmail.com>
2024-12-17 20:22:04 +0100

    ok

--- pipe/mi_execcmd.c
+++ pipe/mi_execcmd.c
@@ -15,11 +15,26 @@
 int	mi_execcmd(t_cmd *mi_cmd, t_sys *mi_sys)
 {
 	char	*errormsg;
-
+	int 	i;
+	i = 0;
+	while (mi_sys->env[i])
+	{
+		dprintf(2, "mi_execcmd env %s \n",mi_sys->env[i]);
+		i++;
+	}
+	
+	i = 0;
+	while (mi_cmd->args[i])
+	{
+		dprintf(2, "mi_execcmd args %s \n",mi_cmd->args[i]);
+		i++;
+	}
+	
 	if (mi_cmd->args[0] == NULL )
 		return (1);
 	if (mi_cmd->builtin == true)
 	{
+		// les argirmnt doivent se terminer par un null
 		mi_execbuiltin(mi_cmd, STDOUT_FILENO, mi_sys);
 		mi_freesys(mi_sys);
 		return (127);
