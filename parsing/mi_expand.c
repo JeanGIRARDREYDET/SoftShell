aefe75c146ffdef989e5b38e8d8aa724bf1b3f8d
JeanGIRARDREYDFET <cogitafac@gmail.com>
2025-01-09 19:43:40 +0100

    Malloc expand

--- parsing/mi_expand.c
+++ parsing/mi_expand.c
@@ -65,8 +65,7 @@ void	mi_expand_find_env(char **full_cmd, int i, int len, t_sys *mi_sys)
 	if (!find)
 		replace = ft_strsubreplace(full_cmd[0], i, 1, "");
 	else
-		replace = ft_strsubreplace(full_cmd[0], i, len, find);		
-	replace = ft_strsubreplace(full_cmd[0], i, len, find);
+		replace = ft_strsubreplace(full_cmd[0], i, len, find);
 	free(*full_cmd);
 	*full_cmd = replace;
 }
