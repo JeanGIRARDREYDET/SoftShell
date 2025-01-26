b198339d75a0f7450de0b35d015c902276a46282
JeanGIRARDREYDFET <cogitafac@gmail.com>
2025-01-26 22:32:16 +0100

    ok

--- pipe/mi_execcmd.c
+++ pipe/mi_execcmd.c
@@ -35,6 +35,8 @@ int	mi_execcmd(t_cmd *mi_cmd, t_sys *mi_sys)
 	else if (execve(mi_cmd->args[0], mi_cmd->args, mi_sys->env) == -1)
 	{
 		mi_logerror2(123, mi_cmd->args[0], "command not found 37", mi_sys);
+		if (access(mi_cmd->args[0], X_OK) != 0) 
+       		mi_freecmdsysexit(0, 126, mi_sys);
 		mi_freecmdsysexit(0, 127, mi_sys);
 	}
 	return (122);
