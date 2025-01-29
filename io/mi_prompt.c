49f3757dd475f49f2c245c027e407b9f99ef5c24
cogitafac@gmail.com <cogitafac@gmail.com>
2025-01-29 18:41:14 +0100

    ok 145

--- io/mi_prompt.c
+++ io/mi_prompt.c
@@ -14,46 +14,41 @@
 
 char	*mi_prompt_text(t_sys *mi_sys)
 {
-	char	*prompt_text;
+	char	*prompt_text2;
 	char	*code_error;
 
 	code_error = ft_itoa(mi_sys->code_error);
-	prompt_text = ft_strjoin(code_error, " SoftShell>");
+	prompt_text2 = ft_strjoin(code_error, " SoftShell>");
 	free(code_error);
-	return (prompt_text);
-}
-
-void	mi_freelineprompexit(t_sys *mi_sys)
-{
-	free(mi_sys->input);
-	mi_freecmdsysexit(1, 0, mi_sys);
+	return (prompt_text2);
 }
 
 void	mi_prompt(t_sys *mi_sys)
 {
 	char	*line;
-	char	*prompt_text;
+//	char	*prompt_text2;
 
-	prompt_text = mi_prompt_text(mi_sys);
+///	prompt_text2 = "toto";
 	while (1)
 	{
-		line = readline(prompt_text);
+		line = readline("MicroShell>");
 		if (line == NULL)
-			mi_freelineprompexit(mi_sys);
+			mi_freecmdsysexit(1, 0, mi_sys);;
 		if (*line == '\0')
 			continue ;
 		mi_sys->input = ft_strtrim(line, WSPACE);
 		free(line);
 		if (!mi_sys->input)
-			free(prompt_text);
-		if (!mi_sys->input)
-			mi_freelineprompexit(mi_sys);
+		{
+			//free(prompt_text2);
+			mi_freecmdsysexit(1, 0, mi_sys);
+		}
 		else if (*mi_sys->input == '\0')
 		{
 			free(mi_sys->input);
 			continue ;
 		}
-		free(prompt_text);
+		//free(prompt_text2);
 		break ;
 	}
 }
