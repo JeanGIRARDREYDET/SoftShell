9ccb6b25b72c70a61fa0c46fe0f9660129489759
cogitafac@gmail.com <cogitafac@gmail.com>
2025-01-31 21:16:03 +0100

    ok

--- check/mi_checksyntax.c
+++ check/mi_checksyntax.c
@@ -12,20 +12,49 @@
 
 #include "../minishell.h"
 
-void	mi_checksyntax(char *line, t_sys *mi_sys)
+bool    mi_checksyntax2(char *line)
 {
-	if (!line)
-		return ;
-	if (ft_strlen(line) == 1 && (*line == '|' || *line == '<' || *line == '>'))
-		mi_logerror(2, "syntax error near unexpected token", mi_sys);
-	else if (ft_strrchr(line, '|') != NULL && (ft_strrchr(line, '|') == line
-			|| *(ft_strrchr(line, '|') + 1) == '\0' || *(ft_strrchr(line, '|')
-				+ 1) == '|'))
-		mi_logerror(2, "syntax error near unexpected token `|'", mi_sys);
-	else if (ft_strrchr(line, '>') != NULL && (*(ft_strrchr(line, '>')
-				+ 1) == '\0' || *(ft_strrchr(line, '>') + 1) == '>'))
-		mi_logerror(2, "syntax error near unexpected token `>'", mi_sys);
-	else if (ft_strrchr(line, '<') != NULL && (*(ft_strrchr(line, '<')
-				+ 1) == '\0' || *(ft_strrchr(line, '<') + 1) == '<'))
-		mi_logerror(2, "syntax error near unexpected token `<'", mi_sys);
+    int i;
+    char *new_line;
+    
+    i = 0;
+    
+    new_line = ft_strdup(line);
+    if (!new_line)
+        return (false);
+    while(new_line[i])
+    {
+        if ((size_t)i <= (ft_strlen(new_line) - 1))
+        {
+            if ((new_line[i] == '>' && (new_line[i + 1] == '<' || new_line[i + 1] == '|')) ||
+                (new_line[i] == '<' && (new_line[i + 1] == '|' || new_line[i + 1] == '>')))
+            {
+                free(new_line);
+                return (true);
+            }
+        }
+        i++;
+    }
+    free(new_line);
+    return(false);
+}
+
+void    mi_checksyntax(char *line, t_sys *mi_sys)
+{
+    if (!line || !*line) 
+        return ;
+    if (mi_checksyntax2(line) == true)
+        mi_logerror(2, "syntax error near unexpected token", mi_sys);
+    else if (ft_strlen(line) == 1 && (*line == '|' || *line == '<' || *line == '>'))
+        mi_logerror(2, "syntax error near unexpected token", mi_sys);
+    else if (ft_strrchr(line, '|') != NULL && (ft_strrchr(line, '|') == line
+            || *(ft_strrchr(line, '|') + 1) == '\0' || *(ft_strrchr(line, '|')
+                + 1) == '|'))
+        mi_logerror(2, "syntax error near unexpected token `|'", mi_sys);
+    else if (ft_strrchr(line, '>') != NULL && (*(ft_strrchr(line, '>')
+                + 1) == '\0' || *(ft_strrchr(line, '>') + 1) == '>'))
+        mi_logerror(2, "syntax error near unexpected token `>'", mi_sys);
+    else if (strrchr(line, '<') != NULL && (*(strrchr(line, '<')
+                + 1) == '\0' || *(ft_strrchr(line, '<') + 1) == '<'))
+        mi_logerror(2, "syntax error near unexpected token `<'", mi_sys);
 }
